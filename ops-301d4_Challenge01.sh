87f82c68c2fd76730b5f316a3172ee31adef7112
DJ Choi <95465339+DJChoi82@users.noreply.github.com>
2022-03-01 19:34:21 -0800

    push

--- ops-301d4_Challenge01.sh
+++ ops-301d4_Challenge01.sh
@@ -3,55 +3,27 @@
 #DJ Choi
 #3/1/2022
 #Writing a bash script to copy syslog file to current directory and rename with current date and time
-	
 
 #assigning todays date and time to a variable
 today=$(date +%Y%m%d%H%M%S)
-	
 
 #output date and time
 echo "current date and time is $today (year month day hour minute second)" 
-	
 
 #copy syslog file to current directory
 cp /var/log/syslog .
-	
 
 #output the file has been copied
 echo syslog file has been copied to current directory
-	
 
 #assigning syslog filename to a variable
 file_name=syslog
-	
-
-#assigning filename and date/time to a variable
-new_filename=$file_name$today#!/bin/bash
-#Class 2 Bash Script
-#DJ Choi
-#3/1/2022
-#Writing a bash script to copy syslog file to current directory and rename with current date and time
-	
-
-#assigning todays date and time to a variable
-today=$(date +%Y%m%d%H%M%S)
-	
-
-#output date and time
-echo "current date and time is $today (year month day hour minute second)" 
-	
-
-#copy syslog file to current directory
-cp /var/log/syslog .
-	
-
-#output the file has been copied
-echo syslog file has been copied to current directory
-	
-
-#assigning syslog filename to a variable
-file_name=syslog
-	
 
 #assigning filename and date/time to a variable
 new_filename=$file_name$today
+
+#renaming the syslog file to add date/time
+mv $file_name $new_filename
+
+#output stating file has been renamed
+echo syslog file has been renamed by adding current date and time
