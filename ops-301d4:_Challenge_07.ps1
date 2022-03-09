63fc05b39598dde2cc812e723e7949ac5a36600a
DJ Choi <djchoi82@gmail.com>
2022-03-09 14:31:42 -0800

    push

---
+++ ops-301d4:_Challenge_07.ps1
@@ -0,0 +1,11 @@
+#class 8 powershell script
+#DJ Choi
+#3/9/2022
+#writing powershell script to add user to AD
+
+#main
+
+#add user
+PS C:\> New-ADUser -Name "Franz Ferdinand" -Title “TPS Reporting Lead” -Company “GlobeX USA” -Office “Springfield, OR” -Department “TPS Department” -EmailAddress “ferdi@GlobeXpower.com”
+
+#end
