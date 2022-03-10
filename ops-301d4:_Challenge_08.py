38a20ec4988990da67e23542674102c861b83f2e
DJ Choi <djchoi82@gmail.com>
2022-03-10 12:54:26 -0800

    push

---
+++ ops-301d4:_Challenge_08.py
@@ -0,0 +1,22 @@
+#!/bin/python3
+#Class 9 Python Script
+#DJ Choi
+#3/10/2022
+#Writing python scri
+
+#assign variable
+list1 = ["carrot", "spinash", "cabbage", "tomato", "celery", "peas", "apple", "corn", "potato", "cucumber"]
+
+#main
+
+#printing from variable list
+print(list1[3])
+print(list1[5:10])
+
+#changing 
+list1[6] = "onion"
+
+#end
+
+
+
